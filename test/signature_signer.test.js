bdfb34c00a1f22f1fa80bff9d33ea348d1dbee0a
moon004 <herodotus94@gmail.com>
2019-10-18 16:15:42 +0900

    Added unit test for EllipticSignatureSigner

--- test/signature_signer.test.js
+++ test/signature_signer.test.js
@@ -10,53 +10,71 @@ describe('Class SignatureSigner', () => {
     sinon.restore();
   });
 
-  // Mock the ArrayBuffertoHex in signer class
-  function genericArrayBuffertoHexMocker() {
-    sinon.replace(jsrsasign, 'ArrayBuffertohex',
-        sinon.fake.returns('hex-value'));
-  }
+  describe('Class JsrsasignSignatureSigner', () => {
+    // Mock the ArrayBuffertoHex in signer class
+    beforeEach(function() {
+      sinon.replace(jsrsasign, 'ArrayBuffertohex',
+          sinon.fake.returns('hex-value'));
+    });
 
-  it('throws exception when pem file is not correct', () => {
-    genericArrayBuffertoHexMocker();
-    const signer = new JsrsasignSignatureSigner('incorrect pem');
-    expect(() => {
+    it('throws exception when pem file is not correct', () => {
+      const signer = new JsrsasignSignatureSigner('incorrect pem');
+      expect(() => {
+        signer.sign('content');
+      }).to.throw('Failed to load private key');
+    });
+
+    it('throws exception when it not is ASN.1 hex string', () => {
+      const signer = new JsrsasignSignatureSigner('mocked non ASN.1 string');
+      expect(() => {
+        signer.sign('content');
+      }).to.throw('Failed to load private key not ASN.1 hex string');
+    });
+
+    it('should work properly', () => {
+      const mockedECDSA = {
+        readPKCS5PrvKeyHex: function() {},
+      };
+      const mockedSignature = {
+        init: function() {},
+        updateHex: function() {},
+        sign: function() {},
+      };
+      const pem = fs.readFileSync(__dirname + '/key.pem', 'utf-8');
+      sinon.replace(jsrsasign.KJUR.crypto, 'ECDSA',
+          sinon.fake.returns(mockedECDSA));
+      sinon.replace(jsrsasign, 'hextoArrayBuffer',
+          sinon.fake.returns([1, 2, 3]));
+      sinon.replace(jsrsasign.KJUR.crypto, 'Signature',
+          sinon.fake.returns(mockedSignature));
+      const signer = new JsrsasignSignatureSigner(pem);
+      const mockSpyECDSA = sinon.spy(mockedECDSA, 'readPKCS5PrvKeyHex');
+      const mockSpySignatureInit = sinon.spy(mockedSignature, 'init');
+      const mockSpySignatureUpdateHex = sinon.spy(mockedSignature, 'updateHex');
+      const mockSpySignatureSign = sinon.spy(mockedSignature, 'sign');
       signer.sign('content');
-    }).to.throw('Failed to load private key');
+      assert(mockSpySignatureInit.calledOnce);
+      assert(mockSpySignatureUpdateHex.withArgs('hex-value').calledOnce);
+      assert(mockSpySignatureSign.calledOnce);
+      assert(mockSpyECDSA.calledOnce);
+    });
   });
 
-  it('throws exception when it not ASN.1 hex string', () => {
-    genericArrayBuffertoHexMocker();
-    const signer = new JsrsasignSignatureSigner('mocked non ASN.1 string');
-    expect(() => {
-      signer.sign('content');
-    }).to.throw('Failed to load private key not ASN.1 hex string');
+  describe('Class EllipticSignatureSigner', () => {
+    it('throws exception when pem file is not correct', () => {
+      const signer = new SignatureSigner('incorrect pem'); // EllipticSignature is used by default
+      expect(() => {
+        signer.sign('content');
+      }).
+          to.
+          throw(
+              'Failed to sign the request unsupported PKCS#1/5 hexadecimal key');
+    });
+    it('should work properly', () => {
+      const pem = fs.readFileSync(__dirname + '/key.pem', 'utf-8');
+      const signer = new SignatureSigner(pem);
+      assert.instanceOf(signer.sign('Content'), Uint8Array);
+    });
   });
 
-  it('should work properly', () => {
-    const mockedECDSA = {
-      readPKCS5PrvKeyHex: function() {},
-    };
-    const mockedSignature = {
-      init: function() {},
-      updateHex: function() {},
-      sign: function() {},
-    };
-    const pem = fs.readFileSync(__dirname + '/key.pem', 'utf-8');
-    sinon.replace(jsrsasign.KJUR.crypto, 'ECDSA',
-        sinon.fake.returns(mockedECDSA));
-    sinon.replace(jsrsasign, 'hextoArrayBuffer', sinon.fake.returns([1, 2, 3]));
-    sinon.replace(jsrsasign.KJUR.crypto, 'Signature',
-        sinon.fake.returns(mockedSignature));
-    genericArrayBuffertoHexMocker();
-    const signer = new JsrsasignSignatureSigner(pem);
-    const mockSpyECDSA = sinon.spy(mockedECDSA, 'readPKCS5PrvKeyHex');
-    const mockSpySignatureInit = sinon.spy(mockedSignature, 'init');
-    const mockSpySignatureUpdateHex = sinon.spy(mockedSignature, 'updateHex');
-    const mockSpySignatureSign = sinon.spy(mockedSignature, 'sign');
-    signer.sign('content');
-    assert(mockSpySignatureInit.calledOnce);
-    assert(mockSpySignatureUpdateHex.withArgs('hex-value').calledOnce);
-    assert(mockSpySignatureSign.calledOnce);
-    assert(mockSpyECDSA.calledOnce);
-  });
 });
