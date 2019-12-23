0a07d4bb78c6131e0039ce1d215d7836e3f9333b
Plenty Su <plenty.su@gmail.com>
2019-12-23 14:40:34 +0900

    Revise test cases

--- test/signature_signer.test.js
+++ test/signature_signer.test.js
@@ -5,6 +5,9 @@ const jsrsasign = require('jsrsasign');
 const sinon = require('sinon');
 const fs = require('fs');
 
+const chai = require('chai');
+chai.use(require('chai-as-promised'));
+
 describe('Class SignatureSigner', () => {
   afterEach(function() {
     sinon.restore();
@@ -17,40 +20,38 @@ describe('Class SignatureSigner', () => {
           sinon.fake.returns('hex-value'));
     });
 
-    it('throws exception when pem file is not correct', () => {
+    it('throws exception when pem file is not correct', async () => {
       const signer = new JsrsasignSignatureSigner('incorrect pem');
-      expect(() => {
-        signer.sign('content');
-      }).to.throw('Failed to load private key');
+      await expect(signer.sign('content'))
+          .to.be.rejectedWith('Failed to load private key');
     });
 
-    it('throws exception when it is not an ASN.1 hex string', () => {
+    it('throws exception when it is not an ASN.1 hex string', async () => {
       const signer = new JsrsasignSignatureSigner('mocked non ASN.1 string');
-      expect(() => {
-        signer.sign('content');
-      }).to.throw('Failed to load private key not ASN.1 hex string');
+      await expect(signer.sign('content'))
+          .to.be.rejectedWith('Failed to load private key');
     });
 
-    it('should work properly', () => {
+    it('should work properly', async () => {
       const pem = fs.readFileSync(__dirname + '/key.pem', 'utf-8');
       const signer = new JsrsasignSignatureSigner(pem);
-      assert.instanceOf(signer.sign('Content'), Uint8Array);
+      assert.instanceOf(await signer.sign('Content'), Uint8Array);
     });
   });
 
   describe('Class EllipticSignatureSigner', () => {
-    it('throws exception when the pem file is not correct', () => {
-      const signer = new SignatureSigner('incorrect pem'); // EllipticSignature is used by default
-      expect(() => {
-        signer.sign('content');
-      }).to.throw(
-          'Failed to sign the request unsupported PKCS#1/5 hexadecimal key');
+    it('throws exception when the pem file is not correct', async () => {
+      // EllipticSignature is used by default
+      const signer = new SignatureSigner('incorrect pem');
+
+      await expect(signer.sign('content')).to.be.rejectedWith(
+          'Failed to sign the request'
+      );
     });
-    it('should work properly', () => {
+    it('should work properly', async () => {
       const pem = fs.readFileSync(__dirname + '/key.pem', 'utf-8');
       const signer = new SignatureSigner(pem);
-      assert.instanceOf(signer.sign('Content'), Uint8Array);
+      assert.instanceOf(await signer.sign('Content'), Uint8Array);
     });
   });
-
 });
