01af00bd38060ce3ed096b25a19d22e10e527e3b
Plenty Su <plenty.su@gmail.com>
2020-02-06 16:28:26 +0900

    Implement ContractExecutionResult

--- contract_execution_result.js
+++ contract_execution_result.js
@@ -2,6 +2,28 @@
  * To handle ContractExecutionResponse
  */
 class ContractExecutionResult {
+  /**
+   * @param {Object} result
+   * @param {Array} proofs
+   */
+  constructor(result, proofs) {
+    this.result = result;
+    this.proofs = proofs;
+  }
+
+  /**
+   * @return {Object}
+   */
+  getResult() {
+    return this.result;
+  }
+
+  /**
+   * @return {Array}
+   */
+  getProofs() {
+    return this.proofs;
+  }
 }
 
 module.exports = {
