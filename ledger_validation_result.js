8c58dd798d7c7952eb4ce30137be03bfa817b5c7
Plenty Su <plenty.su@gmail.com>
2020-02-06 23:49:57 +0900

    Implement LedgerValidationResult

--- ledger_validation_result.js
+++ ledger_validation_result.js
@@ -2,6 +2,28 @@
  * To handle LedgerValidationResponse
  */
 class LedgerValidationResult {
+  /**
+   * @param {StatusCode} code
+   * @param {AssetProof} proof
+   */
+  constructor(code, proof) {
+    this.code = code;
+    this.proof = proof;
+  }
+
+  /**
+   * @return {AssetProof}
+   */
+  getProof() {
+    return this.proof;
+  }
+
+  /**
+   * @return {StatusCode}
+   */
+  getCode() {
+    return this.code;
+  }
 }
 
 module.exports = {
