0ecab62c2d6cd605259aa8921e72fa8e865680cf
scalarindetail <scalarindetail@indetail.co.jp>
2020-02-25 20:40:06 +0900

    finished grpc message getter

---
+++ grpc_message_getter.js
@@ -0,0 +1,292 @@
+const {ClientError} = require('./client_error');
+const {EllipticSigner, WebCryptoSigner} = require('./signer');
+const {TextEncoder} = require('./request/builder');
+const {StatusCode} = require('./status_code');
+
+class GRPCMessageGetter {
+  /**
+   * @param {Object} properties
+   */
+  constructor(properties) {
+    /** @const */
+    this.privateKeyPem = this._getRequiredProperty(properties,
+        'scalar.dl.client.private_key_pem');
+    /** @const */
+    this.certPem = this._getRequiredProperty(properties,
+        'scalar.dl.client.cert_pem');
+    /** @const */
+    this.certHolderId = this._getRequiredProperty(properties,
+        'scalar.dl.client.cert_holder_id');
+    /** @const */
+    this.certVersion = properties['scalar.dl.client.cert_version'];
+
+    /** @const */
+    if (this._isNodeJsRuntime()) {
+      this.signer = new EllipticSigner(this.privateKeyPem);
+    } else {
+      this.signer = new WebCryptoSigner(this.privateKeyPem);
+    }
+  }
+
+  /**
+   * @param {Object} properties JSON Object used for setting client properties
+   * @param {string} name the name of the property to get
+   * @return {Object} The client property specified in the @name parameter
+   */
+  _getRequiredProperty(properties, name) {
+    const value = properties[name];
+    if (!value) {
+      throw new ClientError(
+          StatusCode.CLIENT_IO_ERROR,
+          `property '${name}' is required`,
+      );
+    }
+    return value;
+  }
+
+  /**
+   * @return {Uint8Array}
+   */
+  async getCertificateRegistrationRequest() {
+    const contractCertHolderIdEncoded = new TextEncoder('utf-8').encode(
+        this.certHolderId);
+    const contractArgumentEncoded = new TextEncoder('utf-8').encode(
+        this.certVersion);
+    const contractCertPemEncoded = new TextEncoder('utf-8').encode(
+        this.certPem);
+
+    const buffer = new Uint8Array(
+        contractCertHolderIdEncoded.byteLength
+        + contractArgumentEncoded.byteLength
+        + contractCertPemEncoded.byteLength);
+
+    let offset = 0;
+    buffer.set(contractCertHolderIdEncoded, offset);
+    offset += contractCertHolderIdEncoded.byteLength;
+    buffer.set(contractArgumentEncoded, offset);
+    offset += contractArgumentEncoded.byteLength;
+    buffer.set(contractCertPemEncoded, offset);
+
+    return buffer
+  }
+
+  /**
+   * @param {number} functionId of the function
+   * @param {string} functionBinaryName of the function
+   * @param {Uint8Array} functionBytes of the function
+   * @return {Uint8Array}
+   */
+  async getFunctionRegistrationRequest(functionId, functionBinaryName,
+      functionBytes) {
+    if (!(functionBytes instanceof Uint8Array)) {
+      throw new ClientError(
+          StatusCode.CLIENT_IO_ERROR,
+          'parameter functionBytes is not a \'Uint8Array\'',
+      );
+    }
+    const functionIdEncoded = new TextEncoder('utf-8').encode(
+        functionId.toString());
+    const functionBinaryNameEncoded = new TextEncoder('utf-8').encode(
+        functionBinaryName);
+
+    const buffer = new Uint8Array(
+        functionIdEncoded.byteLength + functionBinaryNameEncoded.byteLength
+        + functionBytes.byteLength);
+
+    let offset = 0;
+    buffer.set(functionIdEncoded, offset);
+    offset += functionIdEncoded.byteLength;
+    buffer.set(functionBinaryNameEncoded, offset);
+    offset += functionBinaryNameEncoded.byteLength;
+    buffer.set(functionBytes, offset);
+
+    return buffer
+  }
+
+  /**
+   * @param {number} contractId of the function
+   * @param {string} contractBinaryName of the function
+   * @param {Uint8Array} contractBytesCode of the function
+   * @param {object} properties of the function
+   * @return {Uint8Array}
+   */
+  async getContractRegistrationRequest(contractId, contractBinaryName,
+      contractBytesCode, properties) {
+    if (!(contractBytesCode instanceof Uint8Array)) {
+      throw new ClientError(
+          StatusCode.CLIENT_IO_ERROR,
+          'parameter contractBytes is not a \'Uint8Array\'',
+      );
+    }
+
+    const contractIdEncoded = new TextEncoder('utf-8').encode(
+        contractId.toString());
+    const contractBinaryNameEncoded = new TextEncoder('utf-8').encode(
+        contractBinaryName);
+    const contractPropertiesEncoded = new TextEncoder('utf-8').encode(
+        JSON.stringify(properties));
+    const contractCertHolderIdEncoded = new TextEncoder('utf-8').encode(
+        this.certHolderId);
+    const contractCertVersionEncoded = new TextEncoder('utf-8').encode(
+        this.certVersion);
+    const buffer = new Uint8Array(
+        contractIdEncoded.byteLength
+        + contractBinaryNameEncoded.byteLength
+        + contractBytesCode.byteLength
+        + contractPropertiesEncoded.byteLength
+        + contractCertHolderIdEncoded.byteLength
+        + contractCertVersionEncoded.byteLength);
+
+    let offset = 0;
+    buffer.set(contractIdEncoded, offset);
+    offset += contractIdEncoded.byteLength;
+    buffer.set(contractBinaryNameEncoded, offset);
+    offset += contractBinaryNameEncoded.byteLength;
+    buffer.set(contractBytesCode, offset);
+    offset += contractBytesCode.byteLength;
+    buffer.set(contractPropertiesEncoded, offset);
+    offset += contractPropertiesEncoded.byteLength;
+    buffer.set(contractCertHolderIdEncoded, offset);
+    offset += contractCertHolderIdEncoded.byteLength;
+    buffer.set(contractCertVersionEncoded, offset);
+    offset += contractCertVersionEncoded.byteLength;
+
+    const signatureEncoded = new TextEncoder('utf-8').encode(
+        await this.signer.sign(buffer));
+
+    const resizedBuffer = new Uint8Array(offset + signatureEncoded.byteLength);
+    resizedBuffer.set(buffer);
+    resizedBuffer.set(signatureEncoded, offset);
+
+    return resizedBuffer
+  }
+
+  /**
+   * @return {Uint8Array}
+   */
+  async getContractListingRequest(contractId) {
+    const contractCertHolderIdEncoded = new TextEncoder('utf-8').encode(
+        this.certHolderId);
+    const contractCertVersionEncoded = new TextEncoder('utf-8').encode(
+        this.certVersion);
+    const contractIdEncoded = new TextEncoder('utf-8').encode(
+        contractId);
+    const buffer = new Uint8Array(
+        contractCertHolderIdEncoded.byteLength
+        + contractCertVersionEncoded.byteLength
+        + contractIdEncoded.byteLength);
+
+    let offset = 0;
+    buffer.set(contractCertHolderIdEncoded, offset);
+    offset += contractCertHolderIdEncoded.byteLength;
+    buffer.set(contractCertVersionEncoded, offset);
+    offset += contractCertVersionEncoded.byteLength;
+    buffer.set(contractIdEncoded, offset);
+    offset += contractIdEncoded.byteLength;
+
+    const signatureEncoded = new TextEncoder('utf-8').encode(
+        await this.signer.sign(buffer));
+
+    const resizedBuffer = new Uint8Array(offset + signatureEncoded.byteLength);
+    resizedBuffer.set(buffer);
+    resizedBuffer.set(signatureEncoded, offset);
+
+    return resizedBuffer
+  }
+
+  /**
+   * @param {number} contractId
+   * @param {Object} contractArgument
+   * @param {Object} [functionArgument=undefined]
+   * @return {Uint8Array}
+   */
+  async getContractExecutionRequest(contractId, contractArgument,
+      functionArgument) {
+    const contractIdEncoded = new TextEncoder('utf-8').encode(
+        contractId.toString());
+    const contractArgumentEncoded = new TextEncoder('utf-8').encode(
+        JSON.stringify(contractArgument));
+    const contractCertHolderIdEncoded = new TextEncoder('utf-8').encode(
+        this.certHolderId);
+    const contractCertVersionEncoded = new TextEncoder('utf-8').encode(
+        this.certVersion);
+    const functionArgumentEncoded = new TextEncoder('utf-8').encode(
+        JSON.stringify(functionArgument));
+    const buffer = new Uint8Array(
+        contractIdEncoded.byteLength
+        + contractArgumentEncoded.byteLength
+        + contractCertHolderIdEncoded.byteLength
+        + contractCertVersionEncoded.byteLength
+        + functionArgumentEncoded.byteLength);
+
+    let offset = 0;
+
+    buffer.set(contractIdEncoded, offset);
+    offset += contractIdEncoded.byteLength;
+    buffer.set(contractArgumentEncoded, offset);
+    offset += contractArgumentEncoded.byteLength;
+    buffer.set(contractCertHolderIdEncoded, offset);
+    offset += contractCertHolderIdEncoded.byteLength;
+    buffer.set(contractCertVersionEncoded, offset);
+    offset += contractCertVersionEncoded.byteLength;
+    buffer.set(functionArgumentEncoded, offset);
+    offset += functionArgumentEncoded.byteLength;
+
+    const signatureEncoded = new TextEncoder('utf-8').encode(
+        await this.signer.sign(buffer));
+
+    const resizedBuffer = new Uint8Array(offset + signatureEncoded.byteLength);
+    resizedBuffer.set(buffer);
+    resizedBuffer.set(signatureEncoded, offset);
+
+    return resizedBuffer
+  }
+
+  /**
+   * @param {number} [assetId]
+   * @return {Uint8Array}
+   */
+  async getLedgerValidationRequest(assetId) {
+    const assetIdEncoded = new TextEncoder('utf-8').encode(
+        assetId.toString());
+    const contractCertHolderIdEncoded = new TextEncoder('utf-8').encode(
+        this.certHolderId);
+    const contractCertVersionEncoded = new TextEncoder('utf-8').encode(
+        this.certVersion);
+    const buffer = new Uint8Array(
+        assetIdEncoded.byteLength
+        + contractCertHolderIdEncoded.byteLength
+        + contractCertVersionEncoded.byteLength);
+
+    let offset = 0;
+
+    buffer.set(assetIdEncoded, offset);
+    offset += assetIdEncoded.byteLength;
+    buffer.set(contractCertHolderIdEncoded, offset);
+    offset += contractCertHolderIdEncoded.byteLength;
+    buffer.set(contractCertVersionEncoded, offset);
+    offset += contractCertVersionEncoded.byteLength;
+
+    const signatureEncoded = new TextEncoder('utf-8').encode(
+        await this.signer.sign(buffer));
+
+    const resizedBuffer = new Uint8Array(offset + signatureEncoded.byteLength);
+    resizedBuffer.set(buffer);
+    resizedBuffer.set(signatureEncoded, offset);
+
+    return resizedBuffer
+  }
+
+  /**
+   *
+   * @return {boolean} true if the runtime is Node.js
+   * @private
+   */
+  _isNodeJsRuntime() {
+    return typeof window === 'undefined';
+  }
+}
+
+module.exports = {
+  GRPCMessageGetter,
+};
---
+++ test/grpc_message_getter.test.js
@@ -0,0 +1,310 @@
+const {
+  ClientError,
+} = require('..');
+const {GRPCMessageGetter} = require('../grpc_message_getter');
+const {TextEncoder} = require('../request/builder');
+
+const sinon = require('sinon');
+const {EllipticSigner} = require("../signer");
+const assert = require('chai').assert;
+const clientProperties = {
+  'scalar.dl.client.private_key_pem': 'key',
+  'scalar.dl.client.cert_pem': 'cert',
+  'scalar.dl.client.cert_holder_id': 'hold',
+  'scalar.dl.client.cert_version': '1.0',
+};
+
+describe('Class GRPCMessageGetter', () => {
+  describe('The constructor', () => {
+    describe('should throw an error', () => {
+      it('when the private key is missing', () => {
+        // prepare
+        const clientProperties = {
+          // "scalar.dl.client.private_key_pem": "key",
+          'scalar.dl.client.cert_pem': 'cert',
+          'scalar.dl.client.cert_holder_id': 'hold',
+        };
+
+        // act assert
+        assert.throws(() => {
+          new GRPCMessageGetter(clientProperties);
+        }, ClientError, 'private_key_pem');
+      });
+      it('when the certificate is missing', () => {
+        // prepare
+        const clientProperties = {
+          'scalar.dl.client.private_key_pem': 'key',
+          // 'scalar.dl.client.cert_pem': 'cert',
+          'scalar.dl.client.cert_holder_id': 'hold',
+        };
+
+        // act assert
+        assert.throws(() => {
+          new GRPCMessageGetter(clientProperties);
+        }, ClientError, 'cert_pem');
+      });
+      it('when holder id is missing', () => {
+        // prepare
+        const clientProperties = {
+          'scalar.dl.client.private_key_pem': 'key',
+          'scalar.dl.client.cert_pem': 'cert',
+          // 'scalar.dl.client.cert_holder_id': 'hold',
+        };
+
+        // act assert
+        assert.throws(() => {
+          new GRPCMessageGetter(clientProperties);
+        }, ClientError, 'cert_holder_id');
+      });
+    });
+    it('should properly load the attribute according to the given property',
+        () => {
+          // prepare
+          const clientProperties = {
+            'scalar.dl.client.private_key_pem': 'key',
+            'scalar.dl.client.cert_pem': 'cert',
+            'scalar.dl.client.cert_holder_id': 'hold',
+            'scalar.dl.client.cert_version': '1.0',
+          };
+
+          // act
+          const gRPCMessageGetter = new GRPCMessageGetter(clientProperties);
+
+          // assert
+          assert.equal(gRPCMessageGetter.privateKeyPem, 'key');
+          assert.equal(gRPCMessageGetter.certPem, 'cert');
+          assert.equal(gRPCMessageGetter.certHolderId, 'hold');
+          assert.equal(gRPCMessageGetter.certVersion, '1.0');
+        });
+  });
+  describe('The method', () => {
+    const mockedReturns = 'mockedReturns';
+    let gRPCMessageGetter;
+
+    afterEach(function () {
+      sinon.restore();
+    });
+
+    beforeEach(function () {
+      const clientProperties = {
+        'scalar.dl.client.private_key_pem': 'key',
+        'scalar.dl.client.cert_pem': 'cert',
+        'scalar.dl.client.cert_holder_id': 'hold',
+        'scalar.dl.client.cert_version': '1.0',
+      };
+
+      gRPCMessageGetter = new GRPCMessageGetter(clientProperties)
+    });
+
+    /**
+     * Mock for the signer library
+     * @param {GRPCMessageGetter} getter
+     */
+    function genericEllipticSignatureSigner(getter) {
+      sinon.replace(getter.signer, 'sign',
+          sinon.fake.returns(mockedReturns));
+    }
+
+    /**
+     * @return {Uint8Array}
+     * @param properties, object that contains all the required object to mock the buffer
+     */
+    function genericBufferArrayGenerator(properties) {
+      let bufferSize = 0;
+      let valueArray = [];
+
+      Object.values(properties).forEach((value) => {
+        if (value.constructor === String) {
+          value = new TextEncoder('utf-8').encode(value);
+        } else if (value.constructor === Number) {
+          value = new TextEncoder('utf-8').encode(value.toString());
+        } else if (value.constructor === Object) {
+          value = new TextEncoder('utf-8').encode(JSON.stringify(value));
+        } else if (value instanceof EllipticSigner) {
+          value = new TextEncoder('utf-8').encode(mockedReturns);
+          bufferSize += value.byteLength;
+          valueArray.push(value);
+          return
+        }
+
+        bufferSize += value.byteLength;
+        valueArray.push(value)
+      });
+
+      const buffer = new Uint8Array(bufferSize);
+      let offset = 0;
+      valueArray.forEach((value) => {
+        if (value instanceof EllipticSigner) {
+          buffer.set(value, offset);
+        } else {
+          buffer.set(value, offset);
+          offset += value.byteLength;
+        }
+      });
+
+      return buffer
+    }
+
+    describe('getCertificateRegistrationRequest', () => {
+      it('should work as expected', async () => {
+        // prepare
+        const mockedProperties = {
+          mockedCertHolderId: clientProperties['scalar.dl.client.cert_holder_id'],
+          mockedCertVersion: clientProperties['scalar.dl.client.cert_version'],
+          mockedCertPem: clientProperties['scalar.dl.client.cert_pem'],
+        };
+        const mockedBuffer = genericBufferArrayGenerator(mockedProperties);
+
+        // act
+        const binaryArray = await gRPCMessageGetter.getCertificateRegistrationRequest();
+
+        // assert
+        assert.deepEqual(binaryArray, mockedBuffer);
+      });
+    });
+
+    describe('getFunctionRegistrationRequest', () => {
+      it('should work as expected', async () => {
+        // prepare
+        const mockedFunctionId = 1234;
+        const mockedFunctionBinaryName = 'mockedFunctionBinaryName';
+        const mockedFunctionBytes = new Uint8Array([1, 2, 3]);
+        const mockedProperties = {
+          mockedFunctionId: mockedFunctionId,
+          mockedFunctionBinaryName: mockedFunctionBinaryName,
+          mockedFunctionBytes: mockedFunctionBytes,
+        };
+
+        const mockedBuffer = genericBufferArrayGenerator(mockedProperties);
+
+        // act
+        const binaryArray = await gRPCMessageGetter.getFunctionRegistrationRequest(
+            mockedFunctionId, mockedFunctionBinaryName, mockedFunctionBytes);
+
+        // assert
+        assert.deepEqual(binaryArray, mockedBuffer);
+      });
+    });
+
+    describe('getContractRegistrationRequest', () => {
+      it('should work as expected', async () => {
+        // prepare
+        const mockedContractId = 1234;
+        const mockedContractBinaryName = 'mockedFunctionBinaryName';
+        const mockedContractBytesCode = new Uint8Array([1, 2, 3]);
+        const mockedContractProperties = {
+          A: 'a',
+          B: 'b'
+        };
+        const mockedSignature = new EllipticSigner(
+            clientProperties['scalar.dl.client.private_key_pem']);
+        const mockedProperties = {
+          mockedContractId: mockedContractId,
+          mockedContractBinaryName: mockedContractBinaryName,
+          mockedContractBytesCode: mockedContractBytesCode,
+          mockedContractProperties: mockedContractProperties,
+          certHolderId: clientProperties['scalar.dl.client.cert_holder_id'],
+          certVersion: clientProperties['scalar.dl.client.cert_version'],
+          mockedSignature: mockedSignature,
+        };
+        const mockedBuffer = genericBufferArrayGenerator(mockedProperties);
+
+        genericEllipticSignatureSigner(gRPCMessageGetter);
+
+        // act
+        const binaryArray = await gRPCMessageGetter.getContractRegistrationRequest(
+            mockedContractId, mockedContractBinaryName, mockedContractBytesCode,
+            mockedContractProperties);
+
+        // assert
+        assert.deepEqual(binaryArray, mockedBuffer);
+      });
+    });
+
+    describe('getContractListingRequest', () => {
+      it('should work as expected', async () => {
+        // prepare
+        const mockedContractId = 1234;
+        const mockedSignature = new EllipticSigner(
+            clientProperties['scalar.dl.client.private_key_pem']);
+        const mockedProperties = {
+          certHolderId: clientProperties['scalar.dl.client.cert_holder_id'],
+          certVersion: clientProperties['scalar.dl.client.cert_version'],
+          mockedContractId: mockedContractId,
+          mockedSignature: mockedSignature,
+        };
+        const mockedBuffer = genericBufferArrayGenerator(mockedProperties);
+
+        genericEllipticSignatureSigner(gRPCMessageGetter);
+
+        // act
+        const binaryArray = await gRPCMessageGetter.getContractListingRequest(
+            mockedContractId);
+
+        // assert
+        assert.deepEqual(binaryArray, mockedBuffer);
+      });
+    });
+
+    describe('getContractExecutionRequest', () => {
+      it('should work as expected', async () => {
+        // prepare
+        const mockedContractId = 1234;
+        const mockedContractArgument = {
+          A: 'a',
+          B: 'b'
+        };
+        const mockedFunctionArgument = {
+          C: 'c',
+          D: 'd'
+        };
+        const mockedSignature = new EllipticSigner(
+            clientProperties['scalar.dl.client.private_key_pem']);
+        const mockedProperties = {
+          mockedContractId: mockedContractId,
+          mockedContractArgument: mockedContractArgument,
+          certHolderId: clientProperties['scalar.dl.client.cert_holder_id'],
+          certVersion: clientProperties['scalar.dl.client.cert_version'],
+          mockedFunctionArgument: mockedFunctionArgument,
+          mockedSignature: mockedSignature,
+
+        };
+        const mockedBuffer = genericBufferArrayGenerator(mockedProperties);
+
+        genericEllipticSignatureSigner(gRPCMessageGetter);
+
+        // act
+        const binaryArray = await gRPCMessageGetter.getContractExecutionRequest(
+            mockedContractId, mockedContractArgument, mockedFunctionArgument);
+
+        // assert
+        assert.deepEqual(binaryArray, mockedBuffer);
+      });
+    });
+
+    describe('getLedgerValidationRequest', () => {
+      it('should work as expected', async () => {
+        // prepare
+        const mockedAssetId = 1234;
+        const mockedSignature = new EllipticSigner(
+            clientProperties['scalar.dl.client.private_key_pem']);
+        const mockedProperties = {
+          mockedAssetId: mockedAssetId,
+          certHolderId: clientProperties['scalar.dl.client.cert_holder_id'],
+          certVersion: clientProperties['scalar.dl.client.cert_version'],
+          mockedSignature: mockedSignature,
+        };
+        const mockedBuffer = genericBufferArrayGenerator(mockedProperties);
+
+        genericEllipticSignatureSigner(gRPCMessageGetter);
+
+        // act
+        const binaryArray = await gRPCMessageGetter.getLedgerValidationRequest(
+            mockedAssetId);
+
+        // assert
+        assert.deepEqual(binaryArray, mockedBuffer);
+      });
+    });
+  });
+});
