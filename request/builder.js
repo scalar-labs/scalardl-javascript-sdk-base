d850ece195510381efa13b821481746ee46ce4fd
scalarindetail <scalarindetail@indetail.co.jp>
2020-03-09 17:20:30 +0900

    Minor Change on builder, and added build functions to ClientServiceBase

--- request/builder.js
+++ request/builder.js
@@ -317,25 +317,25 @@ class ContractsListingRequestBuilder {
    */
   async build() {
     const request = this.request;
+    request.setContractId(this.contractId);
     request.setCertHolderId(this.certHolderId);
     request.setCertVersion(this.certVersion);
-    request.setContractId(this.contractId);
 
+    const contractIdEncoded = new TextEncoder('utf-8').encode(this.contractId);
     const certHolderId = new TextEncoder('utf-8').encode(this.certHolderId);
     const view = new DataView(new ArrayBuffer(4));
     view.setUint32(0, this.certVersion);
     const certVersion = new Uint8Array(view.buffer);
-    const contractIdEncoded = new TextEncoder('utf-8').encode(this.contractId);
 
     const buffer = new Uint8Array(
-      certHolderId.byteLength + certVersion.byteLength + contractIdEncoded.byteLength);
+      contractIdEncoded.byteLength + certHolderId.byteLength + certVersion.byteLength);
     let offset = 0;
 
+    buffer.set(contractIdEncoded, offset);
+    offset += contractIdEncoded.byteLength;
     buffer.set(certHolderId, offset);
     offset += certHolderId.byteLength;
     buffer.set(certVersion, offset);
-    offset += certVersion.byteLength;
-    buffer.set(contractIdEncoded, offset);
 
     request.setSignature(await this.signer.sign(buffer));
 
@@ -501,7 +501,6 @@ class ContractExecutionRequestBuilder {
     request.setContractArgument(this.contractArgument);
     request.setCertHolderId(this.certHolderId);
     request.setCertVersion(this.certVersion);
-    request.setFunctionArgument(this.functionArgument);
 
     const contractId = new TextEncoder('utf-8').encode(this.contractId);
     const contractArgument = new TextEncoder('utf-8').encode(
@@ -510,11 +509,9 @@ class ContractExecutionRequestBuilder {
     const view = new DataView(new ArrayBuffer(4));
     view.setUint32(0, this.certVersion);
     const certVersion = new Uint8Array(view.buffer);
-    const functionArgumentEncoded = new TextEncoder('utf-8').encode(
-        this.functionArgument);
     const buffer = new Uint8Array(
         contractId.byteLength + contractArgument.byteLength +
-        certHolderId.byteLength + certVersion.byteLength + functionArgumentEncoded.byteLength);
+        certHolderId.byteLength + certVersion.byteLength);
     let offset = 0;
 
     buffer.set(contractId, offset);
@@ -524,8 +521,6 @@ class ContractExecutionRequestBuilder {
     buffer.set(certHolderId, offset);
     offset += certHolderId.byteLength;
     buffer.set(certVersion, offset);
-    offset += certVersion.byteLength;
-    buffer.set(functionArgumentEncoded, offset);
 
     request.setSignature(await this.signer.sign(buffer));
 
