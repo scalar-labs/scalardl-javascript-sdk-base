81bb2b714f3bf71ad71143e3df4fd1b5c359df88
Plenty Su <plenty.su@gmail.com>
2020-04-15 14:41:38 +0900

    Create new class Factory to create signers

--- signer/index.js
+++ signer/index.js
@@ -2,8 +2,32 @@ const {EllipticSigner} = require('./EllipticSigner');
 const {JsrsasignSigner} = require('./JsrsasignSigner');
 const {WebCryptoSigner} = require('./WebCryptoSigner');
 
+/**
+ * Factory to generate signers
+ */
+class Factory {
+  /**
+   * @param {String|CryptoKey} key
+   */
+  constructor(key) {
+    this.key = key;
+  }
+
+  /**
+   * @return {EllipticSigner|WebCryptoSigner}
+   */
+  create() {
+    if (typeof window === 'undefined') { // Node environment
+      return new EllipticSigner(this.key);
+    } else { // Browser environment
+      return new new WebCryptoSigner(this.key);
+    }
+  }
+}
+
 module.exports = {
   EllipticSigner,
   JsrsasignSigner,
   WebCryptoSigner,
+  Factory,
 };
