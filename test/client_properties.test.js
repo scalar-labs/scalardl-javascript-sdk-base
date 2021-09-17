4d736b23669fc97b09ed08dbdc7b8a6eaf71ad72
Plenty Su <plenty.su@gmail.com>
2021-09-17 12:01:30 +0900

    Revise the unit test for the ClientProperty class

--- test/client_properties.test.js
+++ test/client_properties.test.js
@@ -35,15 +35,7 @@ describe('ClientProperties', () => {
 
   it('should throw errors when any property of allOf is not provided', () => {
     expect(() => {
-      new ClientProperties(
-          {
-            'scalar.dl.client.cert_holder_id': 'foo',
-          },
-          [
-            ClientPropertiesField.CERT_HOLDER_ID,
-            ClientPropertiesField.CERT_VERSION,
-          ],
-      );
+      new ClientProperties({}, [ClientPropertiesField.CERT_HOLDER_ID]);
     }).to.throw(Error);
   });
 
@@ -55,7 +47,7 @@ describe('ClientProperties', () => {
         null,
         [
           ClientPropertiesField.CERT_HOLDER_ID,
-          ClientPropertiesField.CERT_VERSION,
+          ClientPropertiesField.CERT_PEM,
         ],
     );
     assert.equal('foo', properties.getCertHolderId());
@@ -116,5 +108,43 @@ describe('ClientProperties', () => {
           ClientPropertiesField.PRIVATE_KEY_CRYPTOKEY,
         ], // oneOf
     )).to.throw(Error);
+  });
+
+  it('should set optional properties with default values', () => {
+    const properties = new ClientProperties({}, [], []);
+
+    assert.equal('localhost', properties.getServerHost());
+    assert.equal(50051, properties.getServerPort());
+    assert.equal(50052, properties.getServerPrivilegedPort());
+    assert.equal('localhost', properties.getAuditorHost());
+    assert.equal(40051, properties.getAuditorPort());
+    assert.equal(40052, properties.getAuditorPrivilegedPort());
+    assert.equal(1, properties.getCertVersion());
+    assert.equal(false, properties.getTlsEnabled());
+    assert.equal(false, properties.getAuditorEnabled());
+  });
+
+  it('should overwrite default values of default properties', () => {
+    const properties = new ClientProperties({
+      'scalar.dl.client.server.host': 'ledger.example.com',
+      'scalar.dl.client.server.port': 80,
+      'scalar.dl.client.server.privileged_port': 8080,
+      'scalar.dl.client.auditor.host': 'auditor.example.com',
+      'scalar.dl.client.auditor.port': 443,
+      'scalar.dl.client.auditor.privileged_port': 4433,
+      'scalar.dl.client.cert_version': 10,
+      'scalar.dl.client.tls.enabled': true,
+      'scalar.dl.client.auditor.enabled': true,
+    }, [], []);
+
+    assert.equal('ledger.example.com', properties.getServerHost());
+    assert.equal(80, properties.getServerPort());
+    assert.equal(8080, properties.getServerPrivilegedPort());
+    assert.equal('auditor.example.com', properties.getAuditorHost());
+    assert.equal(443, properties.getAuditorPort());
+    assert.equal(4433, properties.getAuditorPrivilegedPort());
+    assert.equal(10, properties.getCertVersion());
+    assert.equal(true, properties.getTlsEnabled());
+    assert.equal(true, properties.getAuditorEnabled());
   });
 });
